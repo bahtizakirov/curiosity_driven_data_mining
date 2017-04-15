95dd87746e6385719cef603011f7c4634444f0b3
ian borukhovich <ianborukhovich@ians-MacBook-Pro.local>
2017-04-15 14:33:49 -0400

    seems to properly mate generations and find the top n from the next generation. No sound yet

--- genetic_beats/genetic_beats.py
+++ genetic_beats/genetic_beats.py
@@ -1,68 +1,98 @@
 import pygame as pg
 import time
 import numpy
-
+import os
 pg.mixer.init()
 pg.init()
-pg.mixer.set_num_channels(19)
-
+num_generations = 2
 num_notes = 8
-num_saved_generations = 3
-num_progeny = 5
+num_saved_progeny = 2
+num_progeny = 3
+num_loops = 12
+
 sounds = []
-sounds.append(pg.mixer.sounds("/Users/ianborukhovich/Music/Ableton/Factory Packs/Bomblastic/Samples/Drums/Hihat/ClosedHH\ Electrified\ 04\ 2.aif"))
-sounds.append(pg.mixer.sounds("/Users/ianborukhovich/Music/Ableton/Factory Packs/Bomblastic/Samples/Drums/Kick/Kick\ Acoustified\ 01\ 1.aif"))
-sounds.append(pg.mixer.sounds("/Users/ianborukhovich/Music/Ableton/Factory Packs/Bomblastic/Samples/Drums/Hihat/Snare\ Acoustified\ 01\ 1.aif"))
+#hi_hat = pg.mixer.Sound("./boom.wav")
+pg.mixer.set_num_channels(19)
+#pg.mixer.Sound.play(hi_hat)
+#sounds.append(pg.mixer.Sound("./ClosedHH Electrified 04 2.aif"))
+#sounds.append(pg.mixer.Sound("./Kick Acoustified 01 1.aif"))
+#sounds.append(pg.mixer.Sound("./Snare Acoustified 01 1.aif"))
+sounds.append('hi hat')
+sounds.append('snare')
+sounds.append('kick')
 num_sounds = len(sounds)
-last_generation = numpy.empty
-last_generation.concatenate(numpy.randint(0,1, size=(1,num_sounds*num_notes)))
-last_generation.concatenate(numpy.randint(0,1, size=(1,num_sounds*num_notes)))
+
+def array_of_random_integers(length, max_int):
+    rand_int_array = []
+    for i in range(0,length):
+        rand_int_array.append(numpy.random.randint(0, max_int))
+    return rand_int_array
+
+def create_inital_generation(sounds):
+    last_generation = []
+    for j in range(0, num_generations):
+        current_member = []
+        for i in range(0, num_sounds):
+            current_member = current_member + array_of_random_integers(num_notes,2)
+        last_generation.append(current_member)
+    return last_generation
 
 def create_next_generation(last_generation):
     next_generation = []
     for a in range(0, len(last_generation)):
         for b in range(a, len(last_generation)): 
             #mate current pair to generate progeny
-            next_generation.append(
-                mate_current_pair(last_generation[a], last_generation[b])
-                )
+            next_generation = next_generation + mate_current_pair(last_generation[a], last_generation[b])
     return next_generation
 
 def mate_current_pair(father,mother):
     current_generation_beats = []
     for x in range(0, num_progeny):
         #choose member to donate each beat
+        child_beat = []
         for y in range(0, len(father)):
-            inhereted_beat = get_inhereted_beat(father[y],mother[y])
-            child_beat.append(father_or_mother[beat_number])
+            inhereted_beat = get_inherited_beat(father[y],mother[y])
+            child_beat.append(inhereted_beat)
+        current_generation_beats.append(child_beat)
     return current_generation_beats
 
 def get_inherited_beat(father_beat,mother_beat):
-    random_1_0 = numpy.randint(0,1)
+    random_1_0 = numpy.random.randint(0,2)
     if random_1_0 == 0:
         return father_beat
     else:
         return mother_beat
 
 def identify_fit_progeny(next_generation):
+    next_generation_fitness = []
     for beat in next_generation:
         #play beat to rate fitness
-        play_beat(child_beat)
-        fitness = raw_input("score beat")
-        next_generation_fitness.append(fitness)
-    saved_progeny_indices = find_top_n_indices(next_generation_fitness)
-    return next_generation[saved_progeny_indices]
+        #play_beat(beat)
+        print("beat")
+        fitness = input("score beat")
+        next_generation_fitness.append(int(fitness))
+    saved_progeny_indices = find_top_n_indices(next_generation_fitness, num_saved_progeny)
+    return [next_generation[i] for i in saved_progeny_indices]
 
+def find_top_n_indices(list, n):
+    top_n_indices = sorted(range(len(list)), key=lambda x: list[x])[-n:]
+    return top_n_indices
 def play_beat(beat):
+    print(beat)
     stacked_beat = []
-    for i in range(0, num_sounds*num_notes):
-        stacked_beat_index = i%num_notes
-        for sound in sounds
-            if(note)
+    for x in range(0, num_loops):
+        for i in range(0, num_sounds*num_notes):
+            stacked_beat_index = i%num_notes
+            sound_index = i//num_notes
+            if beat[i]:
+                sounds[sound_index].play
 
 #evolve num_generations
+last_generation = create_inital_generation(sounds)
+
 for x in range(0,num_generations):
     #mate pairs of members of current generation
     next_generation = create_next_generation(last_generation)
     saved_progeny = identify_fit_progeny(next_generation)
     last_generation = saved_progeny
+
